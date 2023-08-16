b103a7a31f30493a2342e5c1fdaebb7d6d11c50b
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-16 16:39:23 +0200

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/06-forms/starter/03-multiple-inputs.jsx';
+import Starter from './tutorial/08-custom-hooks/starter/02-fetch-data.jsx';
 
 function App() {
   return (
